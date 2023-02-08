0145a8e1554af1d785b5e575f52d08ea7aaf832f
Pan1245 <siwach.toprasert@gmail.com>
2023-02-08 19:51:26 +0700

    Updates

---
+++ component---src-pages-index-js-59b32af1bd79b34279c0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgrade_tracker=self.webpackChunkgrade_tracker||[]).push([[678],{6558:function(e,t,r){r.r(t),r.d(t,{Head:function(){return n}});var a=r(7294);t.default=()=>a.createElement("p",null,"Siwach Toprasert");const n=()=>a.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-59b32af1bd79b34279c0.js.map
