4ef9556876eff13394d343156b8eff18441e77e2
Pan1245 <siwach.toprasert@gmail.com>
2023-02-08 19:48:56 +0700

    add config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/Pan1245.github.io",
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+};
